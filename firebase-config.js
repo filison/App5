a2b5d8b7a8720c181d16765214c268aa37c930ab
filison <65769401+filison@users.noreply.github.com>
2025-04-17 19:33:51 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAcg0qqzWJwkxmopkTgzfasMqd3siQNxIU",
+  authDomain: "zvladnuto-4e6cb.firebaseapp.com",
+  projectId: "zvladnuto-4e6cb",
+  storageBucket: "zvladnuto-4e6cb.firebasestorage.app",
+  messagingSenderId: "299881468872",
+  appId: "1:299881468872:web:8c36e215cf40a91748e6fd",
+  measurementId: "G-HZZG7JRYCC"
 };
-
 firebase.initializeApp(firebaseConfig);
