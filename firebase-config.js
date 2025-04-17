5cb20a3df06b20e7129d5af71f623a5ce7b9ab30
filison <65769401+filison@users.noreply.github.com>
2025-04-17 19:36:34 +0200

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,2 +1,10 @@
-;
+const firebaseConfig = {
+  apiKey: "AIzaSyAcg0qqzWJwkxmopkTgzfasMqd3siQNxIU",
+  authDomain: "zvladnuto-4e6cb.firebaseapp.com",
+  projectId: "zvladnuto-4e6cb",
+  storageBucket: "zvladnuto-4e6cb.firebasestorage.app",
+  messagingSenderId: "299881468872",
+  appId: "1:299881468872:web:8c36e215cf40a91748e6fd",
+  measurementId: "G-HZZG7JRYCC"
+};
 firebase.initializeApp(firebaseConfig);
